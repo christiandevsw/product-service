d4a7ff7e88f1f5fe8675f72891fe0c99d5385256
Christian Ramirez Davila <christian.devsw@gmail.com>
2023-09-23 06:37:45 -0500

    guardado de fotos exitoso

--- src/main/java/com/dojo/product/service/app/controller/ProductController.java
+++ src/main/java/com/dojo/product/service/app/controller/ProductController.java
@@ -4,15 +4,21 @@ import com.dojo.product.service.app.model.Category;
 import com.dojo.product.service.app.model.Product;
 import com.dojo.product.service.app.service.CategoryService;
 import com.dojo.product.service.app.service.ProductService;
+import com.dojo.product.service.app.utility.DetailProductMixin;
 import com.dojo.product.service.app.utility.ProductMixin;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +29,7 @@ import java.util.stream.Collectors;
 public class ProductController {
     private ProductService productService;
     private CategoryService categoryService;
+    private static final Logger LOGGER= LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping("/total-products")
     public ResponseEntity<?> totalProducts() {
@@ -50,7 +57,7 @@ public class ProductController {
         if (category == null)
             return new ResponseEntity<String>("No existe una categoria en la BD para el id: ".concat(id.toString()), HttpStatus.OK);
 
-        ObjectMapper objectMapper=new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.addMixIn(Product.class, ProductMixin.class);
         List<Object> Products = productService.listProductsByCategory(id).stream().map(m -> {
             try {
@@ -78,7 +85,14 @@ public class ProductController {
         if (product == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe el producto");
         }
-        return new ResponseEntity<Product>(product, HttpStatus.OK);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.addMixIn(Product.class, DetailProductMixin.class);
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(objectMapper.readTree(objectMapper.writeValueAsString(product)));
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.status(HttpStatus.OK).body("Sin foto");
+        }
+//        return new ResponseEntity<Product>(product, HttpStatus.OK);
     }
 
     @GetMapping("/list-by-price/{price}")
@@ -142,8 +156,18 @@ public class ProductController {
     }
 
 
-    @PostMapping("/new-product")
-    public ResponseEntity<?> createNewProduct(@RequestBody Product product) {
+    @PostMapping(value = "/new-product",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> createNewProduct(Product product, @RequestPart MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                product.setPhoto(file.getBytes());
+            } catch (IOException e) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("error", e.getCause().getMessage());
+                map.put("message", "Ocurrió un error en la BD");
+                return new ResponseEntity<Map<String, Object>>(map, HttpStatus.BAD_REQUEST);
+            }
+        }
         try {
             Product newProduct = productService.save(product);
             return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
@@ -156,7 +180,8 @@ public class ProductController {
     }
 
     @PatchMapping("/update-product/{id}")
-    public ResponseEntity<?> updateCurrentProduct(@RequestBody Product product, @PathVariable Long id) {
+    public ResponseEntity<?> updateCurrentProduct(Product product, @PathVariable Long id, @RequestParam MultipartFile file)
+            throws IOException {
         Product currentProduct;
         try {
             currentProduct = productService.getProductById(id);
@@ -175,7 +200,10 @@ public class ProductController {
         currentProduct.setDescription(product.getDescription());
         currentProduct.setStock(product.getStock());
         currentProduct.setAvailable(product.getAvailable());
-        currentProduct.setPhoto(product.getPhoto());
+        currentProduct.setDscto(product.getDscto());
+        if (!file.isEmpty()) {
+            currentProduct.setPhoto(file.getBytes());
+        }
         Product productUpdated = productService.save(currentProduct);
         return new ResponseEntity<Product>(productUpdated, HttpStatus.CREATED);
     }
---
+++ src/main/java/com/dojo/product/service/app/utility/DetailProductMixin.java
@@ -0,0 +1,7 @@
+package com.dojo.product.service.app.utility;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public abstract class DetailProductMixin {
+    private byte[] photo;
+}
